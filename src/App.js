1910cce30f7ad2bcc615be7e985787960595d2eb
rihanmohammed26 <rihanmohammed26@gmail.com>
2021-09-08 19:58:14 +0530

    Added Portal for error

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ const App = () => {
     });
   };
   return (
-    <div>
+    <React.Fragment>
       <AddUser onAddUser={addUserhandler} />
       <UserList users={userList} />
-    </div>
+    </React.Fragment>
   );
 };
 export default App;
--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -42,7 +42,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <React.Fragment>
       {error && (
         <ErrorModal
           title={error.title}
@@ -69,7 +69,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </React.Fragment>
   );
 };
 export default AddUser;
